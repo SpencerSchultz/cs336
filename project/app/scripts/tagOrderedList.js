65f640dabe375eac9bdd5aeb50c18ec77588bd2b
Spencer Schultz <srs27@hoare.cs.calvin.edu>
2016-12-13 12:10:37 -0500

    sorted alphabetically

--- project/app/scripts/tagOrderedList.js
+++ project/app/scripts/tagOrderedList.js
@@ -12,11 +12,14 @@ module.exports = React.createClass({
       .filter(function(link, i) {
         return (!link.tag1.toLowerCase().indexOf(filterInput))
       })
+      .sort(function(a, b) {
+        return a.nickName > b.nickName ? 1 : a.nickName < b.nickName ? -1 : 0;
+      })
       .map(function(link) {
         return (
           <div className="tagAndList">
             <p>
-                <Link to={'/' + link._id}>Edit</Link>   <tagTag>{link.tag1}</tagTag>   <a href={link.link}>{link.nickName}</a>
+                <Link to={'/' + link._id}>Edit</Link>   <tagTag>{link.tag1}</tagTag>   <a href={link.link} class="nickClass">{link.nickName}</a>
             </p>
             </div>
           );
